44c0ee231a5168a1ecd4a0dbd650a442a5286619
annamalai <1987annamalai@gmail.com>
2026-03-21 22:31:16 +0530

    first commit

---
+++ src/main/java/com/manager/moneymager/MoneymagerApplication.java
@@ -0,0 +1,14 @@
+package com.manager.moneymager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymagerApplication.class, args);
+		System.out.println("MoneymagerApplication started successfully!");
+	}
+
+}
---
+++ src/test/java/com/manager/moneymager/MoneymagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manager.moneymager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
